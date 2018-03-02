0881bd1f4ad038b67bd5f85465b8485022739ccd
Uday Shah <udays1995@gmail.com>
2018-03-02 01:26:45 -0500

    sign up page created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+    before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+    before_action :correct_user,   only: [:edit, :update]
+    before_action :admin_user,     only: :destroy
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            @user.send_activation_email
+            # flash[:info] = "Please check your email to activate your account."
+            redirect_to login_url
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password,
+                                     :password_confirmation)
+    end
+
+    def logged_in_user
+        unless logged_in?
+            store_location
+            flash[:danger] = "Please log in."
+            redirect_to login_url
+        end
+    end
+end
